267f01aa28d6b5287361a5247ab96bc20a54c8c8
Joao Andre Simioni <joao.simioni@canonical.com>
2023-03-19 01:34:29 -0300

    Basic Docs

--- aula3/e_ex02/eco_resolucao.py
+++ aula3/e_ex02/eco_resolucao.py
@@ -2,6 +2,8 @@
 
 import random
 
+# Tile():
+# Base class - can check if it's free and can receive a piece on top of it
 class Tile():
     def __init__(self, id):
         self.id = id
@@ -26,6 +28,8 @@ class Tile():
             raise Exception(f"{self.id} is not free")
         self.on_top_of_me = piece
 
+# Piece():
+# Extendes the base class (Tile) and add the methods to move, check if satisfied, set status and objective
 class Piece(Tile):
     def __init__(self, id, size):
         super().__init__(id)
@@ -44,41 +48,44 @@ class Piece(Tile):
 
     def set_on_top_of(self, item):
         if item == self:
-            raise Exception("On top of self")
-        
-        top = self.on_top_of_me 
+            raise Exception("Can't be on top of myself")       
 
+        # Look up to see if I'm trying to be on top of someone above me
+        top = self.on_top_of_me 
         while top is not None:
             if top == item:
                 raise Exception("Can't be on top of my top or someone above")
             top = top.on_top_of_me
+ 
+        # Tell the item I'm on top of it
+        item.set_on_top_of_me(self)
 
-        item.set_on_top_of_me(self) # Set myself on top of item
-
+        # I moved, so let's free who I was blocking before
         if self.on_top_of is not None:
             self.on_top_of.free()
 
+        # Now let's register who I'm on top of
         self.on_top_of = item
 
+# random_position:
+# Takes all the tiles and pieces and generates a random positioning of the pieces
 def random_position(tiles, pieces):
     all = [ *tiles, *pieces ]
     for piece in pieces:
         print(f'Trying to position {piece}')
-        limit = 10
-        while piece.on_top_of is None:
-            print(f'Attempt {11 - limit}')
-            if limit == 0:
-                print("Giving up")
-                return
-            limit -= 1
-            pick = random.choice(all)
+        limit = 30
+        count = 1
+        for pick in random.sample(all, len(all)):
             try:
                 print(f'\tTrying to place {piece} on top of {pick}')
                 piece.set_on_top_of(pick)
                 print(f'\t{piece} is on top of {pick}\n')
+                break
             except Exception as e:
                 print(f'\t{e}')
 
+# print_status:
+# Print the current positioning (Tiles and Pieces)
 def print_status(tiles, pieces):
     tile_size = max([ piece.size for piece in pieces ])
 
@@ -110,12 +117,23 @@ if __name__ == '__main__':
 
     tiles = []
     for x in range(tile_count):
-        tiles.append(Tile(f'Tile_{x+1}'))
+        tiles.append(Tile(f'Tile{x+1:02}'))
 
     pieces = []
     for x in range(piece_count):
-        pieces.append(Piece(f'Piece_{x+1}', x * 2 + 1))
+        pieces.append(Piece(f'Piece{x+1:02}', x * 2 + 1))
     
+    for x in range(piece_count - 1):
+        pieces[x].set_objective(pieces[x+1])
+    
+    pieces[-1].set_objective(tiles[-1])
+
     random_position(tiles, pieces)
+
+    for piece in pieces:
+        print(f'{piece} has objective {piece.objective}')
+
+    print("\n\n")
+
     print_status(tiles, pieces)
     
