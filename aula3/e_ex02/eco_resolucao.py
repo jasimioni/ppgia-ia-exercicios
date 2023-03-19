ae43eb991f149c075e9492ee0dff2541c92f76eb
Joao Andre Simioni <joao.simioni@canonical.com>
2023-03-19 18:18:09 -0300

    Exercise Done

--- aula3/e_ex02/eco_resolucao.py
+++ aula3/e_ex02/eco_resolucao.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 import random
+import time
+import logging
+import sys
+import argparse
+
+logger = logging.getLogger()
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+# Avoid using strings all the time
+BS, BF, F, S = 'BS', 'BF', 'F', 'S'
 
 # Tile():
 # Base class - can check if it's free and can receive a piece on top of it
 class Tile():
     def __init__(self, id):
         self.id = id
-        self.on_top_of_me = None
+        self.above_me = None
 
     def __repr__(self):
         return self.id
@@ -16,17 +26,17 @@ class Tile():
         return '-' * width
 
     def is_free(self):
-        if self.on_top_of_me is None:
+        if self.above_me is None:
             return True
         return False
     
     def free(self):
-        self.on_top_of_me = None
+        self.above_me = None
     
-    def set_on_top_of_me(self, piece):
+    def set_above_me(self, piece):
         if not self.is_free():
             raise Exception(f"{self.id} is not free")
-        self.on_top_of_me = piece
+        self.above_me = piece
 
 # Piece():
 # Extendes the base class (Tile) and add the methods to move, check if satisfied, set status and objective
@@ -34,7 +44,10 @@ class Piece(Tile):
     def __init__(self, id, size):
         super().__init__(id)
         self.size = size
-        self.on_top_of = None
+        self.below_me = None
+        self.state = ['BS']
+        self.restrictions = set([])
+        self.aggressed = False
 
     def set_objective(self, objective):
         self.objective = objective
@@ -43,46 +56,129 @@ class Piece(Tile):
         return ('#' * self.size).center(width)
 
     def is_satisfied(self):
-        if self.on_top_of == self.objective:
+        if self.below_me == self.objective:
             return True
+        
+    def details(self):
+        return f'{self.id}, Objective: {self.objective}, Restrictions: {self.restrictions}, State: {self.state}, Aggressed: {self.aggressed}'
 
-    def set_on_top_of(self, item):
+    @property
+    def BS(self):
+        return self.state[-1] == BS
+
+    @property
+    def BF(self):
+        return self.state[-1] == BF
+
+    @property
+    def S(self):
+        return self.state[-1] == S
+
+    @property
+    def F(self):
+        return self.state[-1] == F
+
+    def set_BF(self):
+        if not self.BF:
+            self.state.append(BF)
+
+    def set_BS(self):
+        if not self.BS:
+            self.state.append(BS)
+
+    def set_F(self):
+        if not self.F:
+            self.state.append(F)
+
+    def set_S(self):
+        if not self.S:
+            self.state.append(S)
+
+    def clear_restrictions(self):
+        self.restrictions = set([])
+        self.aggressed = False
+
+    def aggress_blocker(self):
+        self.above_me.aggressed = True
+        self.above_me.set_BF()
+        self.above_me.restrictions.update([ self.objective, self ])
+    
+    def move(self, item):
         if item == self:
             raise Exception("Can't be on top of myself")       
 
-        # Look up to see if I'm trying to be on top of someone above me
-        top = self.on_top_of_me 
+        # Look up to see if I'm trying to be on top of someone above_me me
+        top = self.above_me 
         while top is not None:
             if top == item:
-                raise Exception("Can't be on top of my top or someone above")
-            top = top.on_top_of_me
+                raise Exception("Can't be on top of my top or someone above_me")
+            top = top.above_me
  
         # Tell the item I'm on top of it
-        item.set_on_top_of_me(self)
+        item.set_above_me(self)
 
         # I moved, so let's free who I was blocking before
-        if self.on_top_of is not None:
-            self.on_top_of.free()
+        if self.below_me is not None:
+            self.below_me.free()
 
         # Now let's register who I'm on top of
-        self.on_top_of = item
+        self.below_me = item
+
+def teacher_position():
+    tiles = []
+    for x in range(3):
+        tiles.append(Tile(f'T{x+1}'))
+
+    t1, t2, t3 = tiles
+
+    pieces = [
+        Piece('A', 1),
+        Piece('B', 3),
+        Piece('C', 5),
+    ]
+
+    a, b, c = pieces
+
+    a.set_objective(c)
+    b.set_objective(t3)
+    c.set_objective(b)
+
+    a.move(t2)
+    b.move(a)
+    c.move(b)
+
+    return tiles, pieces
 
 # random_position:
 # Takes all the tiles and pieces and generates a random positioning of the pieces
-def random_position(tiles, pieces):
+def random_position(tile_count, piece_count):
+    tiles = []
+    for x in range(tile_count):
+        tiles.append(Tile(f'T{x+1:02}'))
+
+    pieces = []
+    for x in range(piece_count):
+        pieces.append(Piece(f'P{x+1:02}', x * 2 + 1))
+    
+    for x in range(piece_count - 1):
+        pieces[x].set_objective(pieces[x+1])
+    
+    pieces[-1].set_objective(tiles[-1])
     all = [ *tiles, *pieces ]
     for piece in pieces:
-        print(f'Trying to position {piece}')
+        logger.debug(f'Trying to position {piece}')
         limit = 30
         count = 1
         for pick in random.sample(all, len(all)):
             try:
-                print(f'\tTrying to place {piece} on top of {pick}')
-                piece.set_on_top_of(pick)
-                print(f'\t{piece} is on top of {pick}\n')
+                logger.debug(f'\tTrying to place {piece} on top of {pick}')
+                piece.move(pick)
+                logger.debug(f'\t{piece} is on top of {pick}\n')
                 break
             except Exception as e:
-                print(f'\t{e}')
+                logger.debug(f'\t{e}')
+    
+    return tiles, pieces
 
 # print_status:
 # Print the current positioning (Tiles and Pieces)
@@ -96,8 +192,8 @@ def print_status(tiles, pieces):
         new_line = []
         for item in lines[-1]:
             if item is not None:
-                new_line.append(item.on_top_of_me)
-                if item.on_top_of_me is not None:
+                new_line.append(item.above_me)
+                if item.above_me is not None:
                     stop = False
             else:
                 new_line.append(None)
@@ -108,32 +204,120 @@ def print_status(tiles, pieces):
 
     for line in reversed(lines):
         line_str = ' '.join( list(map(lambda x: ' ' * tile_size if x is None else x.fmt(tile_size), line)) )
-        print(line_str)
+        logger.info(line_str)
 
+    #for piece in pieces:
+    #    print(piece.details())
+
+def solver(tiles, pieces, delay=False):
+    all = [ *tiles, *pieces ]
+
+    logger.info("Starting solver - initial status:\n")
+    print_status(tiles, pieces)
+    logger.info('')
+
+    limit = 1
+    while sum([ piece.S for piece in pieces ]) < len(pieces):
+        if delay:
+            time.sleep(1)
+        if limit == 500:
+            break
+        logger.info(f"Execution {limit}")
+        limit += 1
+        for piece in pieces:
+            logger.debug(piece.details())
+            if piece.aggressed and not piece.BF:
+                piece.set_BF()
+            elif piece.BS:
+                if piece.is_free():
+                    # Free to move,trying to move to objective
+                    if piece.objective.is_free():
+                        piece.move(piece.objective)
+                        logger.debug(f"Moving {piece} over {piece.objective}")
+                        piece.set_S()
+                else:
+                    piece.aggress_blocker()
+            elif piece.F:
+                piece.set_BS()
+            elif piece.BF:
+                if piece.is_free():
+                    if piece.objective.is_free() and piece.objective not in piece.restrictions:
+                        piece.move(piece.objective)
+                        logger.debug(f"Moving {piece} over {piece.objective}")
+                        piece.set_S()
+                        piece.clear_restrictions()
+                    else:
+                        for option in all:
+                            if option != piece and option.is_free() and option not in piece.restrictions:
+                                piece.move(option)
+                                logger.debug(f"Moving {piece} over {option}")
+                                piece.set_F()
+                                piece.clear_restrictions()
+                                break
+                else:
+                    piece.aggress_blocker()
+        logger.info('')
+        print_status(tiles, pieces)
+        logger.info('')
 
 if __name__ == '__main__':
-    tile_count = 5
-    piece_count = 15
+    parser = argparse.ArgumentParser()
 
-    tiles = []
-    for x in range(tile_count):
-        tiles.append(Tile(f'Tile{x+1:02}'))
+    parser.add_argument(
+        "--debug",
+        help="Print additional information",
+        action='store_true'
+    )
 
-    pieces = []
-    for x in range(piece_count):
-        pieces.append(Piece(f'Piece{x+1:02}', x * 2 + 1))
+    parser.add_argument(
+        "--class_example",
+        help="Create the Pieces as the class example (3 tiles, 3 pieces)",
+        action='store_true'
+    )
+
+    parser.add_argument(
+        "--tiles",
+        help="Number o tiles to create",
+        default=5
+    )
+
+    parser.add_argument(
+        "--pieces",
+        help="Number o pieces to create",
+        default=9
+    )
+
+    parser.add_argument(
+        "--delay",
+        help="Add a delay between steps",
+        action='store_true'
+    )
+
+    args = parser.parse_args()
+
+    logger.setLevel(logging.INFO)
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
     
-    for x in range(piece_count - 1):
-        pieces[x].set_objective(pieces[x+1])
-    
-    pieces[-1].set_objective(tiles[-1])
-
-    random_position(tiles, pieces)
+    if args.class_example:
+        logger.info(f'Loading default class example')
+        tiles, pieces = teacher_position()
+    else:
+        logger.info(f'Creating random setup with {args.tiles} tiles and {args.pieces} pieces')
+        tiles, pieces = random_position(int(args.tiles), int(args.pieces))
 
     for piece in pieces:
-        print(f'{piece} has objective {piece.objective}')
+        logger.info(f'{piece} has objective {piece.objective}')
 
-    print("\n\n")
+    logger.info("\n\n")
 
+    solver(tiles, pieces, delay=args.delay)
+
+    logger.info("\n#############\nFinal result:\n#############\n")
+
+    for piece in pieces:
+        logger.info(piece.details())
+
+    logger.info('')
     print_status(tiles, pieces)
     
